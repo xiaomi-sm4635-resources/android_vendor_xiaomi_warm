47a52f568fb461311412a63d6828f2df88ac33b4
RazenXAX <Razenxizu@gmail.com>
2026-03-04 11:52:48 +0530

    warm: Include firmware

--- Android.mk
+++ Android.mk
@@ -3,3 +3,24 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),warm)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,e1c4b0281ac1c905fb2e86f0cfba531c0e6e748b)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,2342e4595c73b40a2fac44aa7d68ab9481a200e7)
+$(call add-radio-file-sha1-checked,radio/countrycode.img,8d71c6f2db10499f4ee2e2ad9cefdf87c68e6718)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,82b693b52ba5f456f57e09f0648e77de4e8fb9d7)
+$(call add-radio-file-sha1-checked,radio/dsp.img,d15c94a8dba0479c84b544fcc0341c051110c047)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,625d72ab2658ff916145bcb1af498850e0a20096)
+$(call add-radio-file-sha1-checked,radio/hyp.img,fb1515873887bc48c3d637aeb6f07eef1ce89958)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,e5a3464417e738c83cebee8d51d65e5f275e4e29)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,1536122613daf763e98951f88328f70209ccc6a9)
+$(call add-radio-file-sha1-checked,radio/modem.img,c7fa835e2645946f1e90933d7557ccbfc85e3ce9)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,ac48fd9d381c3dcd231c4f2695477dc3f066f17c)
+$(call add-radio-file-sha1-checked,radio/rpm.img,2e0420a6af34b9418acc9cbebf2fd74faf8e148a)
+$(call add-radio-file-sha1-checked,radio/tz.img,beb783cc8737a49b25c02fa08b7970c0a7ae76d1)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,42bd8ae0c8d6b542f81af369df56d69523c94501)
+$(call add-radio-file-sha1-checked,radio/xbl.img,d9145d6e33f4e7ed2e956b587f10f2de6752fe65)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,520ac70eba61f60604d540554de92488f61c2526)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,21 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    bluetooth \
+    countrycode \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    qupfw \
+    rpm \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config
